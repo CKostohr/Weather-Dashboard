521f5ef35d5d201c2d278dcde5707f2ce7002d3a
Chad Kostohryz <kostc7@gmail.com>
2022-10-10 17:16:36 -0500

    This is adding zip code and city lookup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-var APIKey = "90a5a0fd17c8d2e0dfa9c4cd8c6be0cd"
+var APIKey = "90a5a0fd17c8d2e0dfa9c4cd8c6be0cd";
+var magicBtn = "";
+$(document).ready(function(){
+    var city =$("#city").val();
+    if (!isNaN(city)){
+        magicBtn = "zip"
+    }else{
+        magicBtn = "q"
+    }
+})
