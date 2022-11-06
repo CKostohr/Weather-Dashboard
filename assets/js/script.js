64bbef2a927987588b9973486819c0b1d245cacf
Chad Kostohryz <kostc7@gmail.com>
2022-11-06 13:58:02 -0600

    updating and removing bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
-var APIKey = "90a5a0fd17c8d2e0dfa9c4cd8c6be0cd";
-var magicBtn = "";
-$(document).ready(function(){
-    var city =$("#city").val();
-    if (!isNaN(city)){
-        magicBtn = "zip"
-    }else{
-        magicBtn = "q"
-    }
-})
+const APIKey = "90a5a0fd17c8d2e0dfa9c4cd8c6be0cd"
+const today = moment().format(MM/DD/YYYY)
+
+let 
